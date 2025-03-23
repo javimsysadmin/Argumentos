33c5c43db3def2fea462185947e6abe6e790bd4c
Javier Martos <franciscojavier.martos@murciaeduca.es.com>
2025-03-24 00:26:24 +0100

    COMMIT 2

--- src/Ejemplo01.java
+++ src/Ejemplo01.java
@@ -1,6 +1,7 @@
 public class Ejemplo01 {
     public static void main(String[] args) {
             System.out.println("EL argumento ES DE  " + args[0]);
+            System.out.println("EL SEGUNDO argumento ES DE  " + args[0]);
 
         }
 }
